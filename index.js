8e782467fb232ef2a0e31578b296a845baa62c2c
MarinaCheSte <raggatip@list.ru>
2022-11-24 15:16:42 +0300

    refactor:styles refactored

--- index.js
+++ index.js
@@ -5,20 +5,18 @@ function toggleDropDown() {
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
+  let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("circle");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
